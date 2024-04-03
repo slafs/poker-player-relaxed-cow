c2339e8345b2fac44124a9a927144549478fe7e7
Tom Stark <tomasstark@pm.me>
2024-04-03 11:38:53 +0200

    bump version

--- Player.js
+++ Player.js
@@ -1,14 +1,13 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return "0.2";
   }
 
   static betRequest(gameState, bet) {
     bet(0);
   }
 
-  static showdown(gameState) {
-  }
+  static showdown(gameState) {}
 }
 
 module.exports = Player;
