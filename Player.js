a5716e8fd5018d9bbf0816e274e72490c8311d3c
Tom Stark <tomasstark@pm.me>
2024-04-03 12:21:15 +0200

    something

--- Player.js
+++ Player.js
@@ -1,10 +1,24 @@
 class Player {
   static get VERSION() {
-    return "0.2";
+    return "0.3";
   }
 
   static betRequest(gameState, bet) {
-    bet(gameState.current_buy_in);
+    if (
+      gameState.bet_index === 0 &&
+      (gameState.dealer + 1) % 5 === gameState.in_action
+    ) {
+      bet(gameState.small_blind);
+    }
+
+    if (
+      gameState.bet_index === 0 &&
+      (gameState.dealer + 2) % 5 === gameState.in_action
+    ) {
+      bet(gameState.small_blind * 2);
+    }
+
+    bet(gameState.current_buy_in - gameState.players[gameState.in_action].bet);
   }
 
   static showdown(gameState) {}
