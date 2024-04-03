1b144e34645f96cfc8b0f1b787ceed50782e6705
Tom Stark <tomasstark@pm.me>
2024-04-03 15:03:28 +0200

    log

--- Player.js
+++ Player.js
@@ -1,6 +1,6 @@
 class Player {
   static get VERSION() {
-    return "0.8";
+    return "0.9";
   }
 
   static betRequest(gameState, bet) {
@@ -10,6 +10,9 @@ class Player {
       gameState.bet_index === 0 &&
       (gameState.dealer + 1) % 5 === gameState.in_action
     ) {
+      console.log(
+        `[Game: ${gameState.game_id}], round: ${gameState.round}, result: Small blind`
+      );
       return bet(gameState.small_blind);
     }
 
@@ -17,21 +20,37 @@ class Player {
       gameState.bet_index === 0 &&
       (gameState.dealer + 2) % 5 === gameState.in_action
     ) {
+      console.log(
+        `[Game: ${gameState.game_id}], round: ${gameState.round}, result: Big blind`
+      );
       return bet(gameState.small_blind * 2);
     }
 
     const rank = Player.getRank(gameState);
 
+    console.log(
+      `[Game: ${gameState.game_id}], round: ${gameState.round}, rank: ${rank}`
+    );
+
     if (rank > 3) {
+      console.log(
+        `[Game: ${gameState.game_id}], round: ${gameState.round}, result: Raise X2`
+      );
       return bet(
         gameState.current_buy_in - ourPlayer.bet + gameState.minimum_raise * 2
       );
     }
 
     if (gameState.current_buy_in - ourPlayer.bet > ourPlayer.stack) {
+      console.log(
+        `[Game: ${gameState.game_id}], round: ${gameState.round}, result: Fold because of stack size`
+      );
       return bet(0);
     }
 
+    console.log(
+      `[Game: ${gameState.game_id}], round: ${gameState.round}, result: Check/call`
+    );
     return bet(gameState.current_buy_in - ourPlayer.bet);
   }
 
